256bc37b26d9639fdc1da1961f001e41f87c367d
Venkata Ramanjaneyulu Kattamuru <vkattamuru@salesforce.com>
2020-11-22 14:20:57 -0500

    Variables final with comments.

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,23 @@
 //alert('Hello World!'); 
 console.log('Hello World!');
-let age= 27;
+
+let age= 27; //age is a variable which can be changed.
 let year = 1993;
-console.log('age is '+age ,'and year is ' +year);
-age= 30;
-console.log('new age is '+age);
-const pi =2.34;
-console.log('Pi value is '+pi);
-//pi=20; //creates type error if we assign a new value to constant. 
+console.log('age is : '+age ,'and born year is : ' +year);
+
+age= 30;// reassigning the value
+console.log('new age is : '+age);
+
+const pi =2.34; // pi is a constant which can't be changed.
+console.log('Pi value is : '+pi);
+//pi=20; //creates type error if we assign a new value to constant , uncomment and try once.
 //console.log('Pi new value is '+pi); 
+
+var name="Anil-Kattamuru"; // var is a legacy style for declaring variable.
+console.log('Name is : '+name);
+
+/*variable can't be started with digits like 1name
+reserved keywords are not allowed  for identifiers */
+
+
+
