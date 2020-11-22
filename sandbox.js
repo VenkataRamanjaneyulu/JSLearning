cf94c111b3e52f2d1f4d6d16f3c32f5eef41c8f5
Venkata Ramanjaneyulu Kattamuru <vkattamuru@salesforce.com>
2020-11-22 14:11:18 -0500

    variables start

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,11 @@
 //alert('Hello World!'); 
 console.log('Hello World!');
+let age= 27;
+let year = 1993;
+console.log('age is '+age ,'and year is ' +year);
+age= 30;
+console.log('new age is '+age);
+const pi =2.34;
+console.log('Pi value is '+pi);
+//pi=20; //creates type error if we assign a new value to constant. 
+//console.log('Pi new value is '+pi); 
