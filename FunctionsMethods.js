554b9113935e2c7018b63a565079d2bae203145d
Venkata Ramanjaneyulu Kattamuru <vkattamuru@salesforce.com>
2020-11-23 13:44:19 -0500

    Arrow Functions

--- FunctionsMethods.js
+++ FunctionsMethods.js
@@ -53,8 +53,50 @@ let area = calcArea(10);
 console.log('area value is',area);
 
 const doWithArea = function(area){
-    console.log('area passed is', area);
+    console.log('area passed to doWithArea is', area);
 };
 
 doWithArea(area);
+
+
+/* 
+*********** Arrow Functions ***********
+=>Parenthesis and return statement are required if there is one param.
+*/
+const calcArea2 = (radius) => {
+    return (3.14* (radius**2 ) );
+};
+let area2 = calcArea2(10);
+console.log('area value  from arrow function is',area2);
+
+
+/* Converting normal functions to arrow functions.
+const greet = function(){
+    return 'Hello';
+};
+const greetArrow = ()=> 'Hello';
+console.log(greetArrow());
+//return statement and block scope is not required for one statement in function
+
+
+const bill = function(products,tax){
+ let total =0;
+ for(let i=0 ; i<products.length;i++){
+     total += products[i]+(products[i]*tac);
+ }
+ return total;
+};
+
+
+const bill2 = (products,tax) => {
+    let total =0;
+    for(let i=0 ; i<products.length;i++){
+     total += products[i]+(products[i]*tax);
+    }
+ return total;
+};
+
+console.log(bill2([10,30,40],0.1) , 'final price');
+
+*/
 
