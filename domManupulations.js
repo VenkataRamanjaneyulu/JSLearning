ca4e8e41a57eaf9f4ff2108f99e78d0eedfa774d
Venkata Ramanjaneyulu Kattamuru <vkattamuru@salesforce.com>
2020-11-23 16:47:35 -0500

    Query DOM-2

--- domManupulations.js
+++ domManupulations.js
@@ -12,9 +12,19 @@ console.log('para',para);
 
 ////gets mulitiple elements from dom
 const paras = document.querySelectorAll('p');
+// returns node-list
 paras.forEach( (para) =>{
     console.log(para, 'para');
 });
 
+const titl = document.getElementById('page-title');
+console.log(titl,'titl');
 
+const errors = document.getElementsByClassName('error');
+console.log(errors ,'errors');
+// errors is an html collection and forEach method is not supported
+
+const ptags = document.getElementsByTagName('p'); // htmlCollection.
+console.log(ptags);
+console.log(ptags[0]);
 
