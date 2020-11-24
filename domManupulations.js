47c6be50252234c218b66f845612e7ca42e75aaa
Venkata Ramanjaneyulu Kattamuru <vkattamuru@salesforce.com>
2020-11-24 09:41:09 -0500

    Adding and changing content using JS

--- domManupulations.js
+++ domManupulations.js
@@ -28,3 +28,26 @@ const ptags = document.getElementsByTagName('p'); // htmlCollection.
 console.log(ptags);
 console.log(ptags[0]);
 
+
+/* Adding and changing content using JS */
+const para1 = document.querySelector('p');
+//console.log('para text is : ',para1.innerText);
+para1.innerText = 'Hello Venkat!';
+
+paras.forEach( (para) =>{
+    console.log(para.innerText);
+});
+
+
+//changing content
+
+const content = document.querySelector('.content');
+//console.log(content2.innerHTML);
+content.innerHTML += `<h2> This is a new heading from JS  with data</h2>`
+//console.log(content2.innerHTML); /// += is for appending and = for overriding
+
+const persons = ['Venkat','Anil','Andrew','Karuna','Beny','Satyavati'];
+persons.forEach((person) => {
+ content.innerHTML += `<p>${person}</p>`;
+});
+
