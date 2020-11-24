22d3a03915391ac8ec119a8ab4cd2a66eb9eed8b
Venkata Ramanjaneyulu Kattamuru <vkattamuru@salesforce.com>
2020-11-24 14:45:07 -0500

    Adding and Deleting Elements from DOm

--- eventsManupulations.js
+++ eventsManupulations.js
@@ -2,7 +2,7 @@
 
 const button = document.querySelector('button');
 button.addEventListener('click', ()=>{
- //console.log('You Poked me!');
+ console.log('You Poked me!');
 });
 
 const items = document.querySelectorAll('li');
@@ -10,7 +10,25 @@ items.forEach( (item)=>{
    item.addEventListener('click',(evt) => {
     //    console.log(evt.target);
     //    console.log(item);
-    evt.target.style.textDecoration = 'line-through';
+    //evt.target.style.textDecoration = 'line-through';
+    evt.target.remove(); /// removes elemnt from dom
    })
 });
+
+/* 
+***********Adding and Deleting elements from DOM***********
+*/
+let ul = document.querySelector('ul');
+//ul.remove(); // removes dom eelemnt
+
+const button2 =  document.querySelector('button');
+button2.addEventListener('click', ()=>{
+  const li = document.createElement('li');
+  li.textContent = 'someThing new to do';
+  //ul.append(li); // adds at last
+  ul.prepend(li); 
+
+});
+
+// Notice , we can't delete the todo items added by clicking add new button.
 
