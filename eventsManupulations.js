f3e8b83e5703753be708407156a5fc6373f61937
Venkata Ramanjaneyulu Kattamuru <vkattamuru@salesforce.com>
2020-11-25 13:21:50 -0500

    EventBubbling and Delegation

--- eventsManupulations.js
+++ eventsManupulations.js
@@ -1,19 +1,24 @@
 //Events
-
+/*
 const button = document.querySelector('button');
 button.addEventListener('click', ()=>{
  console.log('You Poked me!');
-});
+}); 
+*/
 
+/*
 const items = document.querySelectorAll('li');
 items.forEach( (item)=>{
    item.addEventListener('click',(evt) => {
+    console.log('Li clicked');
     //    console.log(evt.target);
     //    console.log(item);
     //evt.target.style.textDecoration = 'line-through';
     evt.target.remove(); /// removes elemnt from dom
+    evt.stopPropagation(); // stops event bubbling to parent
    })
 });
+*/
 
 /* 
 ***********Adding and Deleting elements from DOM***********
@@ -31,4 +36,15 @@ button2.addEventListener('click', ()=>{
 });
 
 // Notice , we can't delete the todo items added by clicking add new button.
+//Which can be solved using event delegation  / bubbling.
 
+/* 
+********** Event Bubbling and Event Delegation**********
+*/
+
+ul.addEventListener('click',(evt)=>{
+ console.log(evt); // under evt object check for target and then for tagName
+ if(evt.target.tagName ==='LI'){
+     evt.target.remove();
+ }
+});
